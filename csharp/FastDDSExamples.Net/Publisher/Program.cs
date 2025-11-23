3a9295e3dc7838bb2ca8e35b0ed9d574228fece8
Mads Christiansen <mch@eiva.com>
2025-11-23 19:06:33 +0100

    Fix: C# Publisher

--- csharp/FastDDSExamples.Net/Publisher/Program.cs
+++ csharp/FastDDSExamples.Net/Publisher/Program.cs
@@ -1,46 +1,19 @@
-﻿using EMWCore.Interfaces;
-using EMWCore.Models;
+﻿using Eiva.DDSBus;
+using Eiva.DDSBus.Core;
+using FastDDS;
 using IDL;
-
-
-// ----------- EVENT CALLBACKS ---------//
-void OnPublicationMatched(IDDSMatchedStatus status)
-{
-    Console.WriteLine($"[DDS PublicationMatched] Current: {status.CurrentCount} - Total: {status.TotalCount}");
-}
-
-void OnOfferedIncompatibleQos(IDDSIncompatibleQosStatus status)
-{
-    Console.WriteLine($"[DDS OfferedIncompatibleQos] Total: {status.TotalCount} - Policy: {status.LastPolicyId}");
-}
-
-void OnOfferedDeadlineMissed(IDDSDeadlineMissedStatus status)
-{
-    Console.WriteLine($"[DDS OfferedDeadlineMissed] Total: {status.TotalCount}");
-}
-
-void OnLivelinessLost(IDDSLivelinessLostStatus status)
-{
-    Console.WriteLine($"[DDS LivelinessLost] Total: {status.TotalCount}");
-}
-
-void OnUnacknowledgedSampleRemoved()
-{
-    Console.WriteLine($"[DDS UnacknowledgedSampleRemoved]!");
-}
+using IDL.EIVA.Game;
+using Publisher = Eiva.DDSBus.Publisher;
 
 // ----------- SETUP ----------- //
-IDDSNode node = EMW.DDS.CreateNode(69, "EIVA/DDSPublisherExample");
 
-EQosSettings qos = EQosSettings.DataWriterQos();
-qos.History.Depth = 5;
+Eiva.DDSBus.Participant node = new Participant(69);
+Publisher publisher = node.CreatePublisher();
+Topic<MoveCommand> topic = node.CreateTopic<IDL.EIVA.Game.MoveCommand>("MCH/EIVA/Game/MoveCommand");
+WriterListener listener = new WriterListener();
+DataWriterQos qos = publisher.GetDefaultDataWriterQos();
+IDataWriter<MoveCommand> writer = publisher.CreateDataWriter(topic, qos, listener);
 
-IDDSPublisher<IDL.EIVA.Game.MoveCommand> publisher = node.CreatePublisher<IDL.EIVA.Game.MoveCommand>("MCH/EIVA/Game/MoveCommand", qos);
-publisher.PublicationMatched += OnPublicationMatched;
-publisher.OfferedIncompatibleQos += OnOfferedIncompatibleQos;
-publisher.OfferedDeadlineMissed += OnOfferedDeadlineMissed;
-publisher.LivelinessLost += OnLivelinessLost;
-publisher.UnacknowledgedSampleRemoved += OnUnacknowledgedSampleRemoved;
 
 
 // ----------- Construct Sample ----------- //
@@ -55,9 +28,18 @@ for (int i = 0; i < 100; i++)
     sample.dir_y(-1.0f);
     //sample.latitude(10.0);
     //sample.id((ulong)i);
-    publisher.Publish(sample);
+    writer.Publish(sample);
 
 }
 
 Console.WriteLine("Press any key to close publisher....");
 Console.ReadKey();
+
+// ----------- EVENT CALLBACKS ---------//
+class WriterListener : Eiva.DDSBus.Core.DataWriterListener
+{
+    public override void OnPublicationMatched(PublicationMatchedStatus info)
+    {
+        Console.WriteLine($"[Publisher]: Currently connected to [ {info.current_count} ] subscribers!");
+    }
+}
